389e05680813d5b2da1f8c423c8c40bd7844d47a
Test Name <9254497089@mail.ru>
2020-11-20 13:38:11 +0300

    Original ToDo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './components/app';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App/>, document.getElementById('root'))
+
+/*
+
+let Clock = (props) => {
+    return (
+        <div>
+         <h1>Часы</h1>
+         <h2>Сейчас {props.date.toLocaleTimeString()}</h2>
+        </div>
+    );
+}
+
+let func = ()=> {
+    ReactDOM.render(<Clock date={new Date()}/>, document.getElementById('root'));
+}
+
+
+setInterval( func, 1000);
+
+*/
